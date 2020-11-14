ce4de5d37f59ff42f14306c4c433d4a7389ffa55
Micuda Andrei <andrei.micuda@gmail.com>
2020-11-14 21:45:02 +0200

    Moved product edit page into partial view

--- TheWardrobe/Controllers/ProductController.cs
+++ TheWardrobe/Controllers/ProductController.cs
@@ -91,5 +91,40 @@ namespace TheWardrobe.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Edit(int productId)
+        {
+            var product = db.Products.Find(productId);
+            var categories = from cat in db.Categories
+                             select cat;
+            ViewBag.Categories = categories;
+            return View(product);
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int productId, Product productUpdated)
+        {
+            try
+            {
+                Product dbProduct = db.Products.Find(productId);
+
+                if (TryUpdateModel(productUpdated))
+                {
+                    dbProduct.ProductName = productUpdated.ProductName;
+                    dbProduct.Description = productUpdated.Description;
+                    dbProduct.Price = productUpdated.Price;
+                    dbProduct.Size = productUpdated.Size;
+                    dbProduct.Quantity = productUpdated.Quantity;
+                    dbProduct.CategoryId = productUpdated.CategoryId;
+                    dbProduct.ImageUrl = productUpdated.ImageUrl;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("ManageProducts");
+            }
+            catch (Exception e)
+            {
+                return View(productUpdated);
+            }
+        }
     }
 }
