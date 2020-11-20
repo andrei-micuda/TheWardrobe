883baef43626aa534dfe4706ed6c8f81253025b2
Micuda Andrei <andrei.micuda@gmail.com>
2020-11-20 17:51:42 +0200

    Moved Review List into partial view, added delete to review

--- TheWardrobe/Controllers/ReviewController.cs
+++ TheWardrobe/Controllers/ReviewController.cs
@@ -20,5 +20,39 @@ namespace TheWardrobe.Controllers
             db.SaveChanges();
             return RedirectToAction("UpdateRating", "Product", new { productId = reviewToAdd.ProductId });
         }
+
+        [HttpPut]
+        public ActionResult Edit(int reviewId, Review reviewUpdated)
+        {
+            try
+            {
+                var dbReview = db.Reviews.Find(reviewId);
+
+                if (TryUpdateModel(reviewUpdated))
+                {
+                    dbReview.ReviewTitle = reviewUpdated.ReviewTitle;
+                    dbReview.ReviewBody = reviewUpdated.ReviewBody;
+                    dbReview.Rating = reviewUpdated.Rating;
+                    dbReview.DateAdded = DateTime.Now;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Show", "Product", new { productId = reviewUpdated.ProductId });
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Show", "Product", new { productId = reviewUpdated.ProductId });
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int reviewId)
+        {
+            var reviewToDelete = db.Reviews.Find(reviewId);
+            db.Reviews.Remove(reviewToDelete);
+            db.SaveChanges();
+
+            return RedirectToAction("Show", "Product", new { productId = reviewToDelete.ProductId });
+        }
     }
 }
